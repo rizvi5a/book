3ddb042ec6cfdf146241086d7d1a0948ac3a911c
syed Rizvi <rizvi5a@yahoo.com>
2021-08-01 00:27:51 -0500

    dep

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+// Runs on prodction
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
